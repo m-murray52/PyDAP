c08dc8fb0e8f3efff769c7c93b29435c4b417de7
Michael Murray <m.murray52@nuigalway.ie>
2021-12-27 20:51:17 +0000

    Added homography using skimage

--- detect_chessboard.py
+++ detect_chessboard.py
@@ -10,60 +10,6 @@ parser.add_argument('--image', type=str, required= False, help='Input image')
 args = parser.parse_args()
 
 
-"""# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
-
-#images = glob.glob('*.jpg')
-#for fname in images:
-img = cv2.imread('/home/michael/Pictures/Screenshot_20211219_013904.png')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    # Find the chess board corners
-ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
-    # If found, add object points, image points (after refining them)
-if ret == True:
-    objpoints.append(objp)
-    corners2 = cv2.cornerSubPix(gray,corners, (7,7), (-1,-1), criteria)
-    imgpoints.append(corners)
-        # Draw and display the corners
-    cv2.drawChessboardCorners(img, (8,6), corners2, ret)
-    cv2.imshow('img', img)
-    cv2.waitKey()
-
-cv2.destroyAllWindows()
-#print(corners)
-print(corners2)
-print(len(corners2))
-print(corners2.shape)
-print(type(corners2))
-
-# Return an array from the elements of a at given indices
-new_array = np.ndarray.take(corners2, [[[0, 1]], [[2, 3]], [[16, 17]], [[18, 19]]])
-#print(corners2[0])
-print(new_array)
-
-# create projection (h, w) = (85, 85)
-x0 = new_array[0][0][0] 
-y0 = new_array[0][0][1]
-
-
-print(new_array[1])
-
-# Target projection 
-projection = [[x0, y0], [x0, y0 +85], [x0 - 85, y0], [x0 -85, y0 +85]]
-
-print(projection)"""
-
 class Chessboard:
     """
     Detects chessboard pattern corners and outputs corrected chessboard 
@@ -94,26 +40,32 @@ class Chessboard:
             objpoints.append(self.objp)
             self.corners2 = cv2.cornerSubPix(self.gray, self.corners, (7,7), (-1,-1), self.criteria)
             imgpoints.append(self.corners)
+
+            # Return an array from the elements of a at given indices
+            self.points_of_interest = np.ndarray.take(self.corners2, [[0, 1], [14, 15], [80, 81], [94, 95]])
+            print(self.corners2[47])
+
         # Draw and display the corners
         cv2.drawChessboardCorners(self.img, (8,6), self.corners2, ret)
         cv2.imshow('img', self.img)
         cv2.waitKey()
+        print(self.corners2)
 
-    def projection(self, height=85, width=85) -> np.ndarray:
-        # Return an array from the elements of a at given indices
-        new_array = np.ndarray.take(self.corners2, [[[0, 1]], [[2, 3]], [[16, 17]], [[18, 19]]])
+    def projection(self, height=83.3, width=83.3) -> np.ndarray:
+        
     
         # create projection, e.g. (h, w) = (85, 85)
         # Co-ordinates of top right corner (will change later to top left)
-        x0 = new_array[0][0][0] 
-        y0 = new_array[0][0][1]
+        x0 = self.points_of_interest[0][0] 
+        y0 = self.points_of_interest[0][1]
 
 
-        print(new_array[1])
+        #print(self.points_of_interest)
 
         # Target projection 
-        projection = [[x0, y0], [x0, y0 + height], [x0 - width, y0], [x0 -width, y0 + height]]
+        projection = np.array([[x0, y0], [x0, y0 + 7*height], [x0 - 5*width, y0], [x0 - 5*width, y0 + 7*height]])
         return projection
 
-chess_img = Chessboard(args.image)
-print(chess_img.projection())
+#chess_img = Chessboard(args.image)
+#print(chess_img.projection())
+#print(type(chess_img.projection()))
--- homography.py
+++ homography.py
@@ -1,5 +1,49 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.patches import Circle
+import cv2
+from numpy.core.fromnumeric import shape
+import detect_chessboard
 from skimage import transform
 from skimage.io import imread, imshow
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.patches import Circle
+
+
+img1 = cv2.imread('/home/michael/Pictures/Screenshot_20211219_013904.png')
+chess_pattern = detect_chessboard.Chessboard('/home/michael/Pictures/Screenshot_20211219_013904.png')
+points_of_interest = chess_pattern.points_of_interest
+projection = chess_pattern.projection()
+print(points_of_interest)
+print(projection)
+print(type(points_of_interest))
+print(type(projection))
+print(shape(points_of_interest))
+print(shape(projection))
+
+tform = transform.estimate_transform('projective', points_of_interest, projection)
+tf_img_warp = transform.warp(img1, tform.inverse, mode = 'symmetric')
+cv2.imshow('img', tf_img_warp)
+cv2.waitKey()
+
+"""color = 'green'
+patches = []
+fig, ax = plt.subplots(1,2, figsize=(15, 10), dpi = 80)
+for coordinates in (points_of_interest + projection):
+    patch = Circle((coordinates[0],coordinates[1]), 10, 
+                    facecolor = color)
+    patches.append(patch)
+for p in patches[:4]:
+    ax[0].add_patch(p)
+ax[0].imshow(img1)
+
+for p in patches[4:]:
+    ax[1].add_patch(p)
+ax[1].imshow(np.ones((img1.shape[0], img1.shape[1])))"""
+
+"""ret1, corners1 = cv2.findChessboardCorners(img1, patternSize)
+ret2, corners2 = cv2.findChessboardCorners(img2, patternSize)
+
+H, _ = cv2.findHomography(corners1, corners2)
+print(H)
+    img1_warp = cv2.warpPerspective(img1, H, (img1.shape[1], img1.shape[0]))"""
